9d07b7695f504442c1c7a34424d63b39b8b17158
Tanja <tavinogradova92@gmail.com>
2020-01-14 16:53:35 +0100

    Scrollbar for the child component created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -27,13 +28,15 @@ class App extends Component {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
         if (this.state.robots.length === 0) {
-            return <h2>Loading...</h2>
+            return <h1>Loading...</h1>
         } else {
         return (
             <div className='tc'>
                 <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={ filterRobots }/>
+                <Scroll>
+                    <CardList robots={ filterRobots }/>
+                </Scroll>
             </div>
             );
         }
