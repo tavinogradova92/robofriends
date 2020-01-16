bd4a8ba3b524a48dca5cd53b7d02dcb61fd77a4b
Tanja <tavinogradova92@gmail.com>
2020-01-16 15:46:13 +0100

    Updates

---
+++ precache-manifest.abd124847d7552b75440dea0df6b6646.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a7e88982905d33440023611946627c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "890a8bbb5944bb1dff7f",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8384c966e4aab4020a06",
+    "url": "/robofriends/static/css/main.37b447b2.chunk.css"
+  },
+  {
+    "revision": "890a8bbb5944bb1dff7f",
+    "url": "/robofriends/static/js/2.c8ed3679.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.c8ed3679.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8384c966e4aab4020a06",
+    "url": "/robofriends/static/js/main.27227cf7.chunk.js"
+  },
+  {
+    "revision": "e6e9e20fe23598f77326",
+    "url": "/robofriends/static/js/runtime-main.d4e5f5ba.js"
+  }
+]);
